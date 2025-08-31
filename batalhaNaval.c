5bc7023db1bb15651a5787ef776aa7e818addb19
JPedro47 <jpbarbosa2905@gmail.com>
2025-08-31 16:58:38 +0000

    criei o tabuleiro selecionando os navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,79 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM 10 //define os dados do tabuleiro
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+void exibirTabuleiro(int tabuleiro[TAM][TAM])
+{
+    printf("\n=== TABULEIRO ===\n");
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+void posicionarNavio(int tabuleiro[TAM][TAM], int x, int y, int tamanho, char orientacao)
+{
+    printf("\nCoordenadas do navio (%c):\n", orientacao);
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (orientacao == 'H')
+        { // Horizontal
+            if (y + i < TAM)
+            {
+                tabuleiro[x][y + i] = 1;
+                printf("(%d, %d)\n", x, y + i);
+            }
+        }
+        else if (orientacao == 'V')
+        { // Vertical
+            if (x + i < TAM)
+            {
+                tabuleiro[x + i][y] = 1;
+                printf("(%d, %d)\n", x + i, y);
+            }
+        }
+        else if (orientacao == 'D')
+        { // Diagonal principal
+            if (x + i < TAM && y + i < TAM)
+            {
+                tabuleiro[x + i][y + i] = 1;
+                printf("(%d, %d)\n", x + i, y + i);
+            }
+        }
+    }
+}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+int main()
+{
+    int tabuleiro[TAM][TAM] = {0};
+    int x, y, tamanho;
+    char orientacao;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf("=== BATALHA NAVAL ===\n");
+    printf("O tabuleiro eh 10x10 (coordenadas de 0 a 9).\n");
+    printf("Orientacoes: H = Horizontal, V = Vertical, D = Diagonal principal\n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Posicionar 3 navios
+    // Escolha o tamanho ds navios
+    // Escolha a posição dos navios
+    for (int n = 1; n <= 3; n++)
+    {
+        printf("\nDigite a coordenada inicial do navio %d (x y): ", n);
+        scanf("%d %d", &x, &y);
+        printf("Digite o tamanho do navio %d: ", n);
+        scanf("%d", &tamanho);
+        printf("Digite a orientacao do navio %d (H/V/D): ", n);
+        scanf(" %c", &orientacao);
+
+        posicionarNavio(tabuleiro, x, y, tamanho, orientacao);
+    }
+
+    // Mostrar tabuleiro completo
+    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
